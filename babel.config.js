90ccc877d096c4cdd39ab2ce1a3a314a985b961e
Amrith R Naik <amrithrnaik@gmail.com>
2025-08-17 09:58:30 +0530

    [fix] fix project configuration files and update dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
-    plugins: [
-      // NativeWind Babel plugin (should come before Reanimated)
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+    ],
+    plugins: [
       // Reanimated plugin must be listed last
       "react-native-reanimated/plugin",
     ],
